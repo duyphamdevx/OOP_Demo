e884bace58485973214eb6800e390531bd210290
duyphamdevx <duy.phmthe@gmail.com>
2025-07-02 07:49:31 +0000

    update

--- pure_java_project/test/TestTime.java
+++ pure_java_project/test/TestTime.java
@@ -1,9 +1,7 @@
 public class TestTime {
     public static void test(){
         Time t = new Time();
-        t.setTime(2, 9, 30);
-        System.out.println("Giờ: " + t.getHour());
-        System.out.println("Phút: " + t.getMinute());
-        System.out.println("Giây: " + t.getSecond());
+        t.setTime(8, 9, 30);
+        System.out.println("Time: " + t.toString());
     }
 }
